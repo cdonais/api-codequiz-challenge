d50968161aa53dc532568107c79c4cc92a1b1273
Carly Donais <carly.donais@gmail.com>
2022-05-12 19:19:47 -0400

    most recent

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,79 @@
 var playerScore=0;
 var timeRemaining=120;
+var questionEl=document.getElementById("question");
+var choicesEl=document.getElementById("choices");
 startButton.addEventListener('click', startQuiz);
-nextButton.addEventListener('click', nextQuestion);
 //click start button to start quiz
 function startQuiz(){
     if (instructions.style.display !=="none"){
         instructions.style.display ="none";
         quizBox.style.display="block"
     };
-function nextQuestion(){
+    showQuestion(question)
+    console.log(questions);
+}    
+    function showQuestion() {   
+        for(var i=0; i<questions.length; i++);
+        questionEl=questions[i];
+}
 
-}
-function chooseAnswer(){
-    
-}
-    if (questions.length>currentQuestionIndex + 1){
-    nextButton.classList.remove('hide')
-    }
     //after submit, show next question
 //if answer was correct update score
-    if (playerChoice=answer) {
-        playerScore=playerScore + 20;
-    }
 //if answer was incorrect subtract timer
-    else {timeRemaining=timeRemaining-10};
 //after last question, give final score
-    var finalScore=playerScore;
 //ask for initials
 //save score to local storage
 //on high scores click, take to page with high scores
-}
+
 //create questions
 
-const questions= [
+var questions= [
     {question: "Which JavaScript data type is always either true or false?",
+     choices: [
+            "String", 
+            "Boolean", 
+            "Array",
+            "Object",
+        ],
+        correctAnswer: 2
+    },
+    {question: "String values must be enclosed in what?",
         choices: [
-            {text:"String", correct: false},
-            {text:"Boolean", correct: true},
-            {text:"Array", correct: false},
-            {text:"Object", correct: false},
-        ]
+            "Quotes",
+            "Curly brackets",
+            "Parentheses",
+            "None of the above",
+        ],
+        correctAnswer:1
+    },
+    {question: "What does JSON stand for?",
+        choices: [
+            "JavaScript Open Now",
+            "JavaScript Open Notation",
+            "JavaScript Object Notation",
+            "JavaScript?! OH, NO!",
+        ],
+        correctAnswer: 3
 
     },
+    {question: "Where is the best place to put a variable you will use in multiple functions?",
+        choices: [
+            "Local Scope",
+            "Global Scope",
+            "Microscope",
+            "Jonathan Schoop",
+        ],
+        correctAnswer:2
+    },
+    {question: "How do you retrieve information from local storage?",
+        choices: [
+            "localStorage.getItem",
+            "localStorage.setItem",
+            "localStorage.betItem",
+            "localStroage.petItem",
+        ],
+        correctAnswer: 1
+    },
+
 ]
-showQuestion()
+    
