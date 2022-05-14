35162469d6c07d8d28f33fce022f5b0fdbf51184
Carly Donais <carly.donais@gmail.com>
2022-05-14 10:35:19 -0400

    updated showquestion and nextquestion functions

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,110 @@
-var playerScore=0;
-var timeRemaining=120;
-var questionEl=document.getElementById("question");
-var choicesEl=document.getElementById("choices");
-startButton.addEventListener('click', startQuiz);
+var playerScore=0
+//var timeRemaining=getElementById("timer");
+let currentQuestion=0;
+const quizEl=document.getElementById("questionContainer");
+const questionEl=document.getElementById("question");
+const choicesEl=document.querySelectorAll(".answer");
+const aText=document.getElementById("aText");
+const bText=document.getElementById("bText");
+const cText=document.getElementById("cText");
+const dText=document.getElementById("dText");
+const nextButton=document.getElementById("nextButton");
+
+startButton.addEventListener('click', startQuiz)
+//set questions
+const questionsArray=[
+    {question: "Which JavaScript data type is always either true or false?",
+                a: "String",
+                b: "Boolean",
+                c: "Object",
+                d: "Array",
+                correct: "b",    
+},
+    {question: "String values must be enclosed in what?",
+                a: "Quotes",
+                b: "Parentheses",
+                c: "Curly Brackets",
+                d: "None of the Above",
+                correct: "a",
+        },
+    {question: "What does JSON stand for?",
+        
+                a: "JavaScript Open Now",
+                b: "JavaScript Object Notation",
+                c: "JavaScript Open Notation",
+                d: "JavaScript?! OH, NO!",
+                correct: "b",
+    },
+    {question: "Where is the best place to put a variable you will use in multiple functions?",
+        
+                a: "Local Scope",
+                b: "Global Scope",
+                c: "Microscope",
+                d: "Jonathan Schoop",
+                correct: "b",
+},
+    {question: "How do you retrieve information from local storage?",
+        
+                a: "Let Item",
+                b: "Pet Item",
+                c: "Set Item",
+                d: "Get Item",
+                correct: "d",
+        },
+];
 //click start button to start quiz
 function startQuiz(){
     if (instructions.style.display !=="none"){
         instructions.style.display ="none";
         quizBox.style.display="block"
-    };
-    showQuestion(question)
-    console.log(questions);
-}    
-    function showQuestion() {   
-        for(var i=0; i<questions.length; i++);
-        questionEl=questions[i];
-}
-
-    //after submit, show next question
-//if answer was correct update score
-//if answer was incorrect subtract timer
-//after last question, give final score
-//ask for initials
+    }
+};
+    showQuestion();
+//begin timer    
+//show first question
+function showQuestion() { 
+        const currentQuestionData=questionsArray[currentQuestion]
+        questionEl.textContent=currentQuestionData.question;
+        aText.textContent=currentQuestionData.a;
+        bText.textContent=currentQuestionData.b;
+        cText.textContent=currentQuestionData.c;
+        dText.textContent=currentQuestionData.d;
+        }
+        //check if answer is correct
+        submitButton.addEventListener("click",checkAnswer)
+            function checkAnswer(){
+            var playerChoice=questionsArray.checked
+        //if answer is correct update player score and show rightAnswer text        
+            if (playerChoice === questionsArray.correct){
+            playerScore=playerScore+20;
+            rightAnswer.style.display="block";
+            }
+        //if answer is wrong deduct time
+             if (playerChoice !== questionsArray.correct){
+                playerScore=playerScore
+                wrongAnswer.style.display="none";
+            timer=timeRemaining-20
+            }
+    }
+            nextButton.addEventListener("click",()=>{
+                    currentQuestion++
+                    console.log(playerScore);
+                    if(currentQuestion<questionsArray.length){
+                        showQuestion ()
+                        if (rightAnswer.style.display="block"){
+                            rightAnswer.style.display="none";
+                        }
+                        if (wrongAnswer.style.display="block"){
+                            wrongAnswer.style.display="none";
+                        }
+                        console.log(playerScore);
+                    }
+                    //after last question give final score and ask for name
+                    else {
+                        window.prompt("Your game has ended. Please enter your name to save your score of " + playerScore)
+                    }
+                })
 //save score to local storage
 //on high scores click, take to page with high scores
 
-//create questions
 
-var questions= [
-    {question: "Which JavaScript data type is always either true or false?",
-     choices: [
-            "String", 
-            "Boolean", 
-            "Array",
-            "Object",
-        ],
-        correctAnswer: 2
-    },
-    {question: "String values must be enclosed in what?",
-        choices: [
-            "Quotes",
-            "Curly brackets",
-            "Parentheses",
-            "None of the above",
-        ],
-        correctAnswer:1
-    },
-    {question: "What does JSON stand for?",
-        choices: [
-            "JavaScript Open Now",
-            "JavaScript Open Notation",
-            "JavaScript Object Notation",
-            "JavaScript?! OH, NO!",
-        ],
-        correctAnswer: 3
-
-    },
-    {question: "Where is the best place to put a variable you will use in multiple functions?",
-        choices: [
-            "Local Scope",
-            "Global Scope",
-            "Microscope",
-            "Jonathan Schoop",
-        ],
-        correctAnswer:2
-    },
-    {question: "How do you retrieve information from local storage?",
-        choices: [
-            "localStorage.getItem",
-            "localStorage.setItem",
-            "localStorage.betItem",
-            "localStroage.petItem",
-        ],
-        correctAnswer: 1
-    },
-
-]
-    
